7e5122545e5263f771244915a4c3e2d2317acf98
KenWR <111475563+KenWR@users.noreply.github.com>
2023-05-02 21:12:27 +0900

    2023/05/02

--- src/BaekJoon10828.java
+++ src/BaekJoon10828.java
@@ -4,7 +4,6 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
 import java.util.Stack;
-import java.util.StringTokenizer;
 
 public class BaekJoon10828{
     public static void main(String[] args) throws IOException{
---
+++ src/BaekJoon9093.java
@@ -0,0 +1,34 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class BaekJoon9093 {
+
+    public static void main(String[] args) throws IOException{
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        
+        
+        int a = Integer.parseInt(br.readLine());
+        
+        while(a-->0){
+
+            String str=br.readLine();
+            //입력받는값
+            String[] str2  =str.split(" ");
+            //입력 받는 값을 띄어쓰기 기준으로 나눠서 배열에 담기
+
+            for(int i=0; i<str2.length; i++){//배열길이만큼 반복(i의 값이 str2.length와 같아질때까지)
+            
+                //str2[i] 라는 한 단어의 순서를 바꿔야함
+                StringBuilder sb = new StringBuilder(str2[i]);            
+    
+                System.out.print(sb.reverse()+" ");
+                    
+            }
+
+        }
+
+    }
+    
+}
